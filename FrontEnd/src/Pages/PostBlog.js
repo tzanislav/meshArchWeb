01f6ce87efa044c1607c45a37fe346fd2b0acf4d
Tzani Laptop <tzanislav@gmail.com>
2024-12-29 18:04:29 +0200

    blog polish 8

--- FrontEnd/src/Pages/PostBlog.js
+++ FrontEnd/src/Pages/PostBlog.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BlogPostForm from '../Components/BlogPostForm';
 import '../CSS/BlogPostForm.css';
+import Padding from '../Components/Padding';
 
 function PostBlog() {
     return (
         <div>
+            <Padding size="200px" />
             <BlogPostForm />
         </div>
     );
