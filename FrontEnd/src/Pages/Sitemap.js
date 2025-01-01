2e5c0c4e6d18d97616f5323f9a101094115f741c
Tzani Laptop <tzanislav@gmail.com>
2025-01-01 15:41:50 +0200

    site name

--- FrontEnd/src/Pages/Sitemap.js
+++ FrontEnd/src/Pages/Sitemap.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Padding from '../Components/Padding';
 const Sitemap = () => {
   return (
     <div>
+        <Padding size="200px"/>
       <h1>Website Sitemap</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
